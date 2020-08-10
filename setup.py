595cb0d140fd062d29ebc16f75c6d5bf83d4dda1
Dyllan <Dyllanjrusher00@gmail.com>
2020-08-10 06:08:12 +0000

    created google shopping scraper.

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='A google based review scraper that analyzes the reviews of products and performs sentiment analysis on them in order to determine the best reviewed products by sentiment instead of just th e highest rated product.',
+    author='Dyllan USher',
+    license='',
+)
